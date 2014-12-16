10729858faa3443f875f6355213007554ad051bb
Ragnar Johannesen <rj@munin.fo>
2014-12-16 12:21:26 +0100

    code formatting

--- dead_reckon.m
+++ dead_reckon.m
@@ -65,14 +65,11 @@ if any([min(size(speed)) min(size(time))] ~= 1) || any([ndims(speed) ndims(time)
 end
 
 %  Ensure column vectors
-
-lat = lat(:);
-long = long(:);
 speed = speed(:);
 time = time(:);
 
 if ~isempty(spdtimes)
-    if any(diff(spdtimes)<=0)
+    if any(diff(spdtimes) <= 0)
             error(['map:' mfilename ':mapError'], 'Times for speed changes must be in time-increasing order')
     end
   spdtimes = spdtimes(:);
@@ -119,31 +116,28 @@ if crschng  % you haven't included course changes yet
     last1 = max(find(spddists<cumdist(length(cumdist))));
 
     if isempty(last1) % first spddist beyond track, just go to end of track, constant (first) speed
-
         spddists = cumdist(length(cumdist));
         spdtimes = time+spddists/speed(1);
 
     elseif last1 == length(spddists) % track extends beyond spddists, stop at last spddists
-
-        last2=max(find(cumdist < spddists(length(spddists)))); %don't let track take you past last defined speed time
+        last2 = max(find(cumdist < spddists(length(spddists)))); %don't let track take you past last defined speed time
 
         if isempty(last2) % first course change is after last speed time, no course changes apply
-                cumdist = [];
+            cumdist = [];
         else % there are course changes before the last speed time
-                cumdist = cumdist(1:last2);
+            cumdist = cumdist(1:last2);
         end
 
     else  %some interior spddist segment contains end of track
-
-        spddists = [spddists(1:last1);cumdist(length(cumdist))];
+        spddists = [spddists(1:last1); cumdist(length(cumdist))];
         timeseg = (cumdist(length(cumdist)) - spddists(last1)) / speed(last1 + 1);
-        spdtimes = [spdtimes(1:last1);spdtimes(last1) + timeseg];
+        spdtimes = [spdtimes(1:last1); spdtimes(last1) + timeseg];
 
     end
 
     % interpolate the times of the course changes under the speed/time profile.
     % if cumdist is empty, so will be crstimes
-    crstimes = interp1([0;spddists],[time;spdtimes],cumdist,'linear');
+    crstimes = interp1([0;spddists], [time; spdtimes], cumdist, 'linear');
 
     % pack up times and distances of dr events -- will sort later
     spdtimes = [crstimes;spdtimes];
@@ -161,10 +155,10 @@ spddists(qndx) = [];
 
 % There are also every-hour-on-the-hour dr requirements.
 % hrtimes is all whole hours during this period.
-hrtimes=(ceil(time + tol):floor(spdtimes(length(spdtimes)) - tol))';
+hrtimes = (ceil(time + tol):floor(spdtimes(length(spdtimes)) - tol))';
 
 % Pack up and reorder with the other dr events into drtime
-drtime=sort([spdtimes;hrtimes]);
+drtime = sort([spdtimes;hrtimes]);
 
 % Where ever two dr events are within the tolerance (3 minutes), remove one
 jndx=find(diff(drtime) < tol);
@@ -177,16 +171,16 @@ end
 % exactly with spddists, which by now are the distances associated with speed
 % and course changes.  This adds hrtimes and leaves out any distances for points
 % removed for tolerance (3 minute) reasons
-drdist = interp1([time;spdtimes],[0;spddists], drtime, 'linear');
+drdist = interp1([time;spdtimes], [0;spddists], drtime, 'linear');
 
 % The distances for each between-dr segment
-distseg=[drdist(1);diff(drdist)];
+distseg=[drdist(1); diff(drdist)];
 
 % we're going to walk through the track.  Start at the initial point
 startlat = lat(1);
 startlong = long(1);
 
-for i=1:length(drtime)
+for i = 1:length(drtime)
     % which course leg are you on
     indx = find(crstimes < drtime(i));
     if isempty(indx) % you're on the first course leg
@@ -198,7 +192,7 @@ for i=1:length(drtime)
 
     indx = indx + 1;
     steer = course(max(indx));
-    [drlat(i,1), drlong(i,1)] =reckon('rh', startlat, startlong, distseg(i) / 60, steer,'degrees');
+    [drlat(i,1), drlong(i,1)] = reckon('rh', startlat, startlong, distseg(i) / 60, steer,'degrees');
     startlat = drlat(i);
     startlong = drlong(i);
 end
